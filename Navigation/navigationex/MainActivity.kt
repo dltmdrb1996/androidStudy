f21a9760d0e37eaa56fefbd15a5078812a3a2f9c
dltmdrb1996 <65164183+dltmdrb1996@users.noreply.github.com>
2021-04-13 22:02:04 +0900

    Update MainActivity.kt

--- Navigation/navigationex/MainActivity.kt
+++ Navigation/navigationex/MainActivity.kt
@@ -14,10 +14,10 @@ class MainActivity : AppCompatActivity() {
         mBinding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        // 네비게이션들을 담는 호스트
+        // 네비게이션들을 담을 레이아웃을 호스트로 생성한다
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_Nav_Container) as NavHostFragment
 
-        // 네비게이션 컨트롤러
+        // 컨트롤러를 설정한다 컨트롤러는 navigation graph의 정보를 기반으로 프래그먼트를 컨트롤한다
         val navController = navHostFragment.navController
 
         // 바텀 네비게이션 뷰 와 네비게이션을 묶어줌
